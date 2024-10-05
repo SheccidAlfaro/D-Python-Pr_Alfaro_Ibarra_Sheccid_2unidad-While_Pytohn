4ace393c7a29fc5ee32d7b4b919dc7aa408e1c23
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-10-04 20:42:07 -0600

    Add files via upload

---
+++ For_Python_Alfaro.py/For_Python.py
@@ -0,0 +1,28 @@
+print("")
+print("Alfaro Ibarra Sheccid 3w For")
+#Mostrar cada fruta de la lista
+frutas=["Platano", "Uva", "Mango"]
+for x in frutas:
+    print(x)
+
+#A traves de las letras "Platano"
+
+for x in "platano":
+    print(x)
+
+for x in "Uva":
+    print(x)
+
+for x in "Mango":
+    print(x)
+
+
+#Salir cuando es "Platano"
+
+frutas=["Platano", "Uva", "Mango"]
+for x in frutas:
+    print(x)
+
+
+if x=="Platano":
+    print(x)
