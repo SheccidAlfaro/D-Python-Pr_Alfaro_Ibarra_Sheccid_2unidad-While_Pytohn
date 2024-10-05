6c2ae0f5e51dd056b3e2a473daafc2aec40f7ac4
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-10-04 20:30:01 -0600

    Add files via upload

---
+++ While_Pytohn/While_Python_Alfaro.py
@@ -0,0 +1,34 @@
+print("")
+print("Alfaro ibarra Sheccid 3w, while do")
+#La variable (i) es tan largo como lo es (i) es menor a 6
+
+i=1
+while i<6:
+    print(i)
+    i+=1
+
+#Salir cuando (i) es igual a 3
+i=1
+
+while i<6:
+    print(i)
+    break
+if i==3:
+    i+=1 
+
+#Continua una vez que i es igual a 3
+i = 0
+while i < 6:
+    i += 1
+    continue
+if i == 3:
+
+    print(i)
+
+#Mensaje desplegado cuando sea falso
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
